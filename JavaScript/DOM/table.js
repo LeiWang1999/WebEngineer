cfe1814321389b416ac082b3c467387e583ef85d
Princeling <2230647190@qq.com>
2019-09-23 21:19:07 +0800

    2019年9月23日

--- JavaScript/DOM/table.js
+++ JavaScript/DOM/table.js
@@ -1,12 +1,50 @@
-window.onload = function(){
+window.onload = function () {
+
+
+
+
+
+
+
+
     var oTable = document.getElementById('table1');
     var oTableBody = oTable.tBodies[0];
 
     var oBtn1 = document.getElementById('btn1');
     var oName = document.getElementById('name');
     var oEdge = document.getElementById('edge');
-    var aTableTr    = oTableBody.rows;
+    var oSex = document.getElementById('sex');
+    var aTableTr = oTableBody.rows;
     var oldColor = '';
+
+
+    function appendTableElement(Id, Name, Sex, Edge, Opration) {
+        var NewChildTr = document.createElement('tr');
+        var NewChildTdId = document.createElement('td');
+        var NewChildTdName = document.createElement('td');
+        var NewChildTdEdge = document.createElement('td');
+        var NewChildTdSex = document.createElement('td');
+        var NewChildTdOpra = document.createElement('td');
+        NewChildTdId.innerText = Id;
+        NewChildTdName.innerText = Name;
+        NewChildTdSex.innerText = Sex;
+        NewChildTdEdge.innerText = Edge;
+        NewChildTdOpra.innerHTML = Opration;
+
+        NewChildTdOpra.onclick = function(){
+            oTableBody.removeChild(this.parentNode);
+        }
+
+        NewChildTr.appendChild(NewChildTdId);
+        NewChildTr.appendChild(NewChildTdName);
+        NewChildTr.appendChild(NewChildTdSex);
+        NewChildTr.appendChild(NewChildTdEdge);
+        NewChildTr.appendChild(NewChildTdOpra);
+        return NewChildTr;
+    }
+
+
+
 
     console.log('接下来打印每个人的姓名');
     for (const key in aTableTr) {
@@ -18,22 +56,26 @@ window.onload = function(){
 
     for (let index = 0; index < aTableTr.length; index++) {
         const element = aTableTr[index];
-        element.onmouseover = function(){
+        element.onmouseover = function () {
             oldColor = this.style.background;
             this.style.background = 'green';
         }
-        element.onmouseout = function(){
+        element.onmouseout = function () {
             this.style.background = oldColor;
         }
-        if (index%2) {
+        if (index % 2) {
             element.style.background = 'red';
         }
     }
 
-    oBtn1.onmousedown = function(){
+    oBtn1.onmousedown = function () {
+        currentId = oTableBody.rows.length + 1;
         currentName = oName.value;
         currentEdge = oEdge.value;
-        var NewChildTr = document.crea
+        currentSex = oSex.value;
+        appendOpra = '<a href="javascript:">删除</a>'
+        var NewChildTr = appendTableElement(currentId, currentName, currentSex, currentEdge, appendOpra);
+        oTableBody.appendChild(NewChildTr);
     }
 
 }
