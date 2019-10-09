a0ed4f338a395485e948dede108a7c0cff97ddf1
Princeling <2230647190@qq.com>
2019-10-09 13:01:31 +0800

    学习了文件流

--- Node.js/文件系统/文件流.js
+++ Node.js/文件系统/文件流.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '\\readMe.txt', 'utf-8');
+var myReadStream = fs.createReadStream(__dirname + '\\readMe.txt');
+var myWriteStream = fs.createWriteStream(__dirname + '\\writeMe.txt');
+myReadStream.setEncoding('utf-8');
+
+var writeData = "Hello world";
+myWriteStream.write(writeData);
+myWriteStream.end();
+myWriteStream.on('finished', function(){
+    console.log('writeFinished');
+});
+
+// 管道是一样的
+// myReadStream.pipe(myWriteStream);
 
 // 常用事件
 // data—有数据可读时触发
@@ -9,5 +21,11 @@ var myReadStream = fs.createReadStream(__dirname + '\\readMe.txt', 'utf-8');
 // finish—所有数据被写入到底层系统时触发
 
 myReadStream.on('data', function(chunk){
+    console.log('触发了数据事件');
     console.log(chunk);
+})
+
+myReadStream.on('end', function(data){
+    console.log('触发了end事件');
+    // console.log(data);
 })
